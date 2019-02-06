a0cd6381bd67f0316cb97d4c2e218206c03abdc4
Christine Kim <ckim31093@gmail.com>
2019-02-06 09:56:25 -0500

    Create webpack files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const canvasEl = document.getElementsByTagName("canvas")[0];
+  canvasEl.width = 100;
+  canvasEl.height = 100;
+
+  const ctx = canvasEl.getContext("2d");
+  
+  // const game = new Game();
+  // new GameView(game, ctx).start();  
+});

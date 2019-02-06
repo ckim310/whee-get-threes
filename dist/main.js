9ae827714ef0a335bac4558b5ba810db677c8450
Christine Kim <ckim31093@gmail.com>
2019-02-06 16:14:12 -0500

    Add index.html and bundled file

--- dist/main.js
+++ dist/main.js
@@ -86,14 +86,51 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/board.js":
+/*!**********************!*\
+  !*** ./src/board.js ***!
+  \**********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return Board; });\n/* harmony import */ var _tile__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./tile */ \"./src/tile.js\");\n\n\nclass Board {\n  constructor(ctx) {\n    this.grid = [\n      [0,0,0,0],\n      [0,0,0,0],\n      [0,0,0,0],\n      [0,0,0,0]\n    ];\n    this.ctx = ctx;\n  }\n\n\n  addNumber() {\n    const empty = [];\n    for (let i = 0; i < this.grid.length; i++) {\n      const row = this.grid[i];\n      for (let j = 0; j < row.length; j++) {\n        const tile = row[j];\n        if (tile === 0) {\n          empty.push({\n            x: i,\n            y: j\n          });\n        }\n      }\n    }\n\n    if (empty.length > 0) {\n      let tileIdx = empty[Math.floor(Math.random() * empty.length)];\n      let r = Math.random();\n      let val;\n      if (r > 0.8) {\n        val = 2;\n      } else if ( r > 0.4 ) {\n        val = 1;\n      } else {\n        val = 3;\n      }\n      // this.grid[tileIdx.x][tileIdx.y] = val;\n      this.grid[tileIdx.x][tileIdx.y] = new _tile__WEBPACK_IMPORTED_MODULE_0__[\"default\"](val, tileIdx.x, tileIdx.y);\n    }\n  }\n\n  moveTile() {\n    \n  }\n\n  draw() {\n    let w = 400;\n    for (let i = 0; i < this.grid.length; i++) {\n      for (let j = 0; j < this.grid.length; j++) {\n        this.ctx.noFill();\n        this.ctx.strokeWeight(2);\n        this.ctx.stroke();\n        this.ctx.rect(i * w, j * w, w, w);\n        \n      }\n      \n    }\n  }\n}\n\n//# sourceURL=webpack:///./src/board.js?");
+
+/***/ }),
+
+/***/ "./src/game.js":
+/*!*********************!*\
+  !*** ./src/game.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return Game; });\n/* harmony import */ var _board__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./board */ \"./src/board.js\");\n\n\nclass Game {\n  constructor(ctx){\n    this.board = new _board__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\n    this.ctx = ctx;\n  }\n\n  setup(){\n    this.board.addNumber();\n    this.board.addNumber();\n    this.board.addNumber();\n    this.board.addNumber();\n    this.board.addNumber();\n    this.board.addNumber();\n    this.board.addNumber();\n    this.board.addNumber();\n    this.board.addNumber();\n\n    this.board.draw();\n  }\n\n}\n\n//# sourceURL=webpack:///./src/game.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("document.addEventListener(\"DOMContentLoaded\", () => {\n  const canvasEl = document.getElementsByTagName(\"canvas\")[0];\n  canvasEl.width = 100;\n  canvasEl.height = 100;\n\n  const ctx = canvasEl.getContext(\"2d\");\n  \n  // const game = new Game();\n  // new GameView(game, ctx).start();  \n});\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _game__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./game */ \"./src/game.js\");\n\n\ndocument.addEventListener(\"DOMContentLoaded\", () => {\n  const canvasEl = document.getElementsByTagName(\"canvas\")[0];\n  canvasEl.width = 400;\n  canvasEl.height = 400;\n\n  const ctx = canvasEl.getContext(\"2d\");\n  \n  // const board = new Board();\n  \n  const game = new _game__WEBPACK_IMPORTED_MODULE_0__[\"default\"](ctx);\n  game.setup();\n  console.table(game.board.grid);\n  // game.fillStyle = \"blue\";\n  // new GameView(game, ctx).start();  \n});\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/tile.js":
+/*!*********************!*\
+  !*** ./src/tile.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return Tile; });\nclass Tile {\n  constructor(value, x, y){\n    this.value = value;\n    this.x = x;\n    this.y = y;\n  }\n}\n\n//# sourceURL=webpack:///./src/tile.js?");
 
 /***/ })
 
